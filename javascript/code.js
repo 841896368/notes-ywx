eea63acf12489448d646663a81423c3b7fb14baf
841896368 <841896368@qq.com>
2021-05-21 23:31:24 +0800

    Update code.js

--- javascript/code.js
+++ javascript/code.js
@@ -804,6 +804,25 @@ function binarySearch(arr, low, high, key) {
   }
 }
 
+// 快速排序 O(nlogn)
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr
+  }
+  var pivotIndex = Math.floor(arr.length / 2)
+  var pivot = arr.splice(pivotIndex, 1)[0]
+  var left = []
+  var right = []
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right))
+}
+
 // 链表
 class Node {
   constructor(key) {
